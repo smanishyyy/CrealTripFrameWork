6f9225e4ee10179360a5258a3189c2fefaa978c0
Manish <manishchaurasia531@gmail.com>
2019-08-25 13:37:39 +0530

    no commit

--- src/main/java/TestCase/FlightBookingTest.java
+++ src/main/java/TestCase/FlightBookingTest.java
@@ -1,4 +1,4 @@
-package TestCase;
+TestCase;
 
 import java.io.File;
 import java.util.List;
---
+++ testbat.bat
@@ -0,0 +1,3 @@
+set projectLocation=C:\D:\Workspace\ClearTripFrameWork\D:\Workspace\ClearTripFrameWork\
+cd %projectLocation%
+mvn clean install -DsuiteXmlFile=testng.xml
